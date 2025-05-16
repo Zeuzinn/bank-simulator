0dca7a263b10b491d7a1ee139288a41ed935ff90
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-16 16:06:20 -0300

    arquivos bancários

--- contas/conta_poupanca.py
+++
@@ -1,7 +0,0 @@
-from conta_bancaria import ContaBancaria
-
-class ContaPoupanca(ContaBancaria):
-    def render_juros(self):
-        rendimento = self.saldo * 0.05
-        self.saldo += rendimento
-        print(f"Juros aplicados: R${rendimento:.2f}")
